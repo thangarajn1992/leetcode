f4326a644a6a6d061b0334ba88f139ad9b79e7cb
Thangaraj N <48826080+thangarajn1992@users.noreply.github.com>
2021-05-02 16:33:42 +0530

    Removed challenged directory
    Since we are documenting all the problems in gitbooks. No need for this directory any more.
    All the problems under this directory are already documented in gitbooks. Any new problems will be directly updated via gitbooks.

--- Challenges/500_600/525_Distribute_Candies/525_Solution.cpp
+++
@@ -1,6 +0,0 @@
-int distributeCandies(vector<int>& candyType) {
-    bitset<200001> hash;
-    for(int i = 0; i < candyType.size(); i++)
-        hash.set(candyType[i]+100000);
-    return min((int)candyType.size()/2, (int)hash.count());     
-}
